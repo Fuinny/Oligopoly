efa382aded3a7713949b6b188e96762b18e896a6
Semion Medvedev <126599206+Fuinny@users.noreply.github.com>
2023-04-12 21:22:30 +0300

    Implemented the main menu of the game.

--- Oligopoly/Program.cs
+++ Oligopoly/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿namespace Oligopoly
+{
+    public class Program
+    {
+        /// <summary>
+        ///  Program entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            RunMainMenu();
+        }
+
+        /// <summary>
+        /// Runs the main menu of the game.
+        /// </summary>
+        private static void RunMainMenu()
+        {
+            string prompt = @"
+ ██████╗ ██╗     ██╗ ██████╗  ██████╗ ██████╗  ██████╗ ██╗  ██╗   ██╗
+██╔═══██╗██║     ██║██╔════╝ ██╔═══██╗██╔══██╗██╔═══██╗██║  ╚██╗ ██╔╝
+██║   ██║██║     ██║██║  ███╗██║   ██║██████╔╝██║   ██║██║   ╚████╔╝ 
+██║   ██║██║     ██║██║   ██║██║   ██║██╔═══╝ ██║   ██║██║    ╚██╔╝  
+╚██████╔╝███████╗██║╚██████╔╝╚██████╔╝██║     ╚██████╔╝███████╗██║   
+ ╚═════╝ ╚══════╝╚═╝ ╚═════╝  ╚═════╝ ╚═╝      ╚═════╝ ╚══════╝╚═╝   
+        --Use up and down arrow keys to select an option--                                                                     
+";
+            string[] options = { "Play", "About", "Exit" };
+
+            Menu mainMenu = new Menu(prompt, options);
+
+            mainMenu.RunMenu();
+        }
+    }
+}
