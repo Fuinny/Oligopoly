1c50281cdf6f52ad9e9d630879b886eca82a1e92
Semion Medvedev <126599206+Fuinny@users.noreply.github.com>
2023-04-13 19:24:35 +0300

    Implemented methods for About and Exit buttons.

--- Oligopoly/Program.cs
+++ Oligopoly/Program.cs
@@ -29,7 +29,56 @@
 
             Menu mainMenu = new Menu(prompt, options);
 
-            mainMenu.RunMenu();
+            int selectedOption = mainMenu.RunMenu();
+
+            switch (selectedOption) 
+            {
+                case 0:
+                    RunStartMenu();
+                    break;
+                case 1:
+                    DisplayAboutInfo();
+                    break;
+                case 2:
+                    ExitGame();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Runs the start menu of the game.
+        /// </summary>
+        private static void RunStartMenu()
+        {
+
+        }
+
+        /// <summary>
+        /// Displays information about the game.
+        /// </summary>
+        private static void DisplayAboutInfo()
+        {
+            Console.Clear();
+            Console.WriteLine(@"THANKS!
+No really, thank you for taking time to play this simple console game. It means a lot.
+
+This game was created by Semion Medvedev (Fuinny)
+My GitHub profile: https://github.com/Fuinny
+
+Press any key to exit the menu...");
+            Console.ReadKey(true);
+            RunMainMenu();
+        }
+
+        /// <summary>
+        /// Exits the game.
+        /// </summary>
+        private static void ExitGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Press any key to exit the game...");
+            Console.ReadKey(true);
+            Environment.Exit(0);
         }
     }
 }
