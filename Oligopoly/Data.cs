73091f79ea09391dd6dbee8659c4bf8746da5a1c
Semion Medvedev <126599206+Fuinny@users.noreply.github.com>
2023-04-16 20:36:42 +0300

    Implemented system for reading .xml file.

---
+++ Oligopoly/Data.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace Oligopoly
+{
+    [XmlRoot("Data")]
+    public class Data
+    {
+        [XmlArray("Companies")]
+        [XmlArrayItem("Company")]
+        public List<Company>? gameCompanies { get; set; }
+
+        [XmlArray("Events")]
+        [XmlArrayItem("Event")]
+        public List<Event>? gameEvents { get; set; }
+    }
+}
---
+++ Oligopoly/Event.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace Oligopoly
+{
+    [XmlRoot("Events")]
+    public class Event
+    {
+        [XmlElement("Effect")]
+        public int Effect { get; set; }
+
+        [XmlElement("Target")]
+        public string? Target { get; set; }
+
+        [XmlElement("Type")]
+        public string? Type { get; set; }
+
+        [XmlElement("Title")]
+        public string? Title { get; set; }
+
+        [XmlElement("Content")]
+        public string? Content { get; set; }
+    }
+}
